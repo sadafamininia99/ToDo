14f254a07b673948c91f403c62a2702bf7d10fb2
sadafamininia99 <aminicomputer96@gmail.com>
2021-09-08 22:02:36 +0430

    cleaner than past

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const form = document.getElementById('form');
-
 const input = document.getElementById('input');
-
-
-
 const todosUL = document.getElementById('todos');
-
-
 
 form.addEventListener('submit' ,(e) =>{
 e.preventDefault();
