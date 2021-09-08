b5c91367da4e751090c14c0f51309e72c3d14a9e
sadafamininia99 <aminicomputer96@gmail.com>
2021-09-08 22:01:46 +0430

    function Todo added

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ const todosUL = document.getElementById('todos');
 
 
 
-form.addEventListener('submit' ,(e) =>){
+form.addEventListener('submit' ,(e) =>{
 e.preventDefault();
 
+
+addTodo();
+})
+
+function addTodo(todo){
+    let todoText =input.value;
+    if(todo){
+        todoText =todo.todoText
+    }
 }
