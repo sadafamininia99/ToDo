dd19b6d81a0943e03a7606630e19016ca4380dcf
sadafamininia99 <aminicomputer96@gmail.com>
2021-09-08 21:58:58 +0430

    DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+
+const input = document.getElementById('input');
+
+
+
+const todosUL = document.getElementById('todos');
+
+
+
+form.addEventListener('submit' ,(e) =>){
+e.preventDefault();
+
+}
