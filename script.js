e2d0a3d6672db5adecb4091e2e15e24c53f09a42
sadafamininia99 <aminicomputer96@gmail.com>
2021-09-13 11:01:26 +0430

    test

--- script.js
+++ script.js
@@ -5,31 +5,15 @@ const todosUL = document.getElementById('todos');
 form.addEventListener('submit' ,(e) =>{
 e.preventDefault();
 
+//prevent the form having the default behaviar
+
 
 addTodo();
 })
 
 function addTodo(todo){
-    let todoText =input.value;
+    let todoText = input.value;
     if(todo){
         todoText =todo.todoText
     }
 }
-
-if(todoText) {
-    const todoEl = document.createElement('li')
-    if(todo && todo.completed) {
-        todoEl.classList.add('completed')
-    }
-    //if 
-
-    todoEl.innerText = todoText
-
-    todoEl.addEventListener('click', () => {
-        todoEl.classList.toggle('completed')
-        updateLS()
-    }) 
-
-
-    todoEl.addEventListener('contextmenu', (e) => {
-        
