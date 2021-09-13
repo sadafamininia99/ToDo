ee1a584b49445281c59ebb796c64559939e759f0
sadafamininia99 <aminicomputer96@gmail.com>
2021-09-13 11:35:03 +0430

    Local storage

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const todosUL = document.getElementById('todos');
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
 
-form.addEventListener('submit' ,(e) =>{
-e.preventDefault();
+const todos = JSON.parse(localStorage.getItem('todos'))
 
-//prevent the form having the default behaviar
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-addTodo();
+    addTodo()
 })
 
+function addTodo(todo) {
+    let todoText = input.value
 
-function addTodo(todo){
-    let todoText = input.value;
-    if(todo){
-
-        todoText = todo.todo.text;
+    if(todo) {
+        todoText = todo.text
     }
-    // console.log(todoText)
-    if(todoText){
-        const todoEl =document.createElement('li')
-        if(todo && todo.completed){
-            todoEl.classList.add('completed');
 
+    if(todoText) {
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed')
         }
-        todoEl.innerText =todoText;
+
+        todoEl.innerText = todoText
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            updateLS()
+        }) 
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        }) 
+
         todosUL.appendChild(todoEl)
-        input.value =""
-         
+
+        input.value = ''
+
+        updateLS()
     }
 }
 
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
